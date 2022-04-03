f1941f42ba37d000ee6ae15e582f27a0e5e470b6
Shani-Sinojiya <shanisinojiya@gmail.com>
2022-04-03 23:28:29 +0530

    add favicon

--- next.config.js
+++ next.config.js
@@ -9,8 +9,13 @@ const nextConfig = {
   },
   env: {
     MONGODB_URI:
-      "mongodb+srv://Shani:HJcdbkPcTgmqIGN9@rm1.vrdzd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    MONGODB_PASSWORD: "HJcdbkPcTgmqIGN9",
+      "mongodb+srv://Shani:Twm9DVtPY9wXoTnx@rm1.ro59o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    MONGODB_PASSWORD: "Twm9DVtPY9wXoTnx",
+    GOOGLE_CLIENT_ID:
+      "856777061385-qo7hvr2bm0vslff4es8p6hcjsclbpq08.apps.googleusercontent.com",
+    GOOGLE_CLIENT_SECRET: "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",
+    GITHUB_CLIENT_ID: "c5e8016f9a8c00422e7f",
+    GITHUB_CLIENT_SECRET: "22b767bed8cdec399eaee867b3b540c0eafeb12a",
   },
 };
 
