0ac3ffda93c9f011da844df611687d7a38603056
Shani-Sinojiya <shanisinojiya@gmail.com>
2022-05-05 16:39:58 +0530

    demo resume theme 1 half

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./themes/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -11,9 +12,26 @@ module.exports = {
       fontFamily: {
         hsi: ["Harlow Solid Italic"],
         mont: ["Montserrat", "sans-serif"],
+        arial: ["Arial", "sans-serif"],
       },
       spacing: {
         "7.75rem": "7.75rem",
+      },
+      width: {
+        "595px": "595px",
+        "120px": "120px",
+      },
+      height: {
+        "60px": "60px",
+      },
+      boxShadow: {
+        "theme-1": "0 2px 4px 0 rgba(16, 18, 20, 0.08)",
+      },
+      colors: {
+        "theme-1-black": "#101214",
+      },
+      lineHeight: {
+        "18px": "18px",
       },
     },
   },
---
+++ themes/Theme_1.tsx
@@ -0,0 +1,183 @@
+import "bootstrap-icons/font/bootstrap-icons.css";
+function Theme_1() {
+  return (
+    <div className="w-595px h-auto font-arial">
+      <hr className="w-full h-1 bg-theme-1-black" />
+      <div className="flex">
+        <div className="w-2/3">
+          <div className="m-12 h-60px text-theme-1-black text-2xl capitalize">
+            <div className="leading-7 font-bold">Shani Sinojiya</div>
+            <div className="leading-7">Senior Product Designer</div>
+          </div>
+          <div className="m-12 h-60px">
+            <div>
+              {/* experience */}
+              <div className="text-[7px] leading-18px uppercase tracking-[2px] text-[#73808D] font-bold">
+                experience
+              </div>
+              <div>
+                <div className="leading-18px font-bold text-theme-1-black text-[11px]">
+                  Senior UI/UX Product Designer
+                </div>
+                <div className="leading-18px font-normal text-theme-1-black text-[11px]">
+                  Enterprise name
+                </div>
+                <div className="text-[9px] leading-18px text-[#73808D] font-normal">
+                  Aug 2018 - Present - 1 year, Paris
+                </div>
+                <div className="leading-18px font-normal text-theme-1-black text-[11px]">
+                  Directly collaborated with CEO and Product team to prototype,
+                  design and deliver the UI and UX experience with a lean design
+                  process: research, design, test, and iterate.
+                </div>
+                <br />
+              </div>
+              <div>
+                <div className="leading-18px font-bold text-theme-1-black text-[11px]">
+                  UI/UX Product Designer
+                </div>
+                <div className="leading-18px font-normal text-theme-1-black text-[11px]">
+                  Enterprise name
+                </div>
+                <div className="text-[9px] leading-18px text-[#73808D] font-normal">
+                  Aug 2013 - Aug 2018 - 5 years, Paris
+                </div>
+                <div className="leading-18px font-normal text-theme-1-black text-[11px]">
+                  Lead the UI design with the accountability of the design
+                  system, collaborated with product and development teams on
+                  core projects to improve product interfaces and experiences.
+                </div>
+                <br />
+              </div>
+              <div>
+                <div className="leading-18px font-bold text-theme-1-black text-[11px]">
+                  UI Designer
+                </div>
+                <div className="leading-18px font-normal text-theme-1-black text-[11px]">
+                  Enterprise name
+                </div>
+                <div className="text-[9px] leading-18px text-[#73808D] font-normal">
+                  Aug 2012 - jul 2013 - 1 year, Paris
+                </div>
+                <div className="leading-18px font-normal text-theme-1-black text-[11px]">
+                  Designed mobile UI applications for Orange R&D departement,
+                  BNP Paribas, La Poste, Le Cned...
+                </div>
+                <br />
+              </div>
+            </div>
+            <div>
+              {/* education */}
+              <div className="text-[7px] leading-18px uppercase tracking-[2px] text-[#73808D] font-bold">
+                education
+              </div>
+              <div>
+                <div className="leading-18px font-bold text-theme-1-black text-[11px]">
+                  Bachelor European in Graphic Design
+                </div>
+                <div className="leading-18px font-normal text-theme-1-black text-[11px]">
+                  School name
+                </div>
+                <div className="text-[9px] leading-18px text-[#73808D] font-normal">
+                  2009 - 2010, Bagnolet
+                </div>
+                <br />
+              </div>
+              <div>
+                <div className="leading-18px font-bold text-theme-1-black text-[11px]">
+                  BTS Communication Visuelle option Multimédia
+                </div>
+                <div className="leading-18px font-normal text-theme-1-black text-[11px]">
+                  School name
+                </div>
+                <div className="text-[9px] leading-18px text-[#73808D] font-normal">
+                  2007 - 2009, Bagnolet
+                </div>
+                <br />
+              </div>
+            </div>
+            <div>
+              {/* Projects */}
+              <div className="text-[7px] leading-18px uppercase tracking-[2px] text-[#73808D] font-bold">
+                Projects
+              </div>
+              <div>
+                <div className="leading-18px font-bold text-theme-1-black text-[11px]">
+                  {/* title */}
+                  Gujrati Study Web
+                </div>
+                <div className="leading-18px font-normal text-theme-1-black text-[11px]">
+                  {/* ḷink */}
+                  <a
+                    href="https://gujratistudyweb.netlify.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    https://gujratistudyweb.netlify.app/
+                  </a>
+                </div>
+                <br />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-1/3">
+          <div>
+            <img
+              src="/img/avatar.svg"
+              className="m-9 rounded-full w-[72px] h-[72px] shadow-theme-1"
+              alt="avatar"
+            />
+          </div>
+          <div className="mt-8 w-120px h-auto font-arial">
+            <div>
+              {/* contect info */}
+              <div className="text-[11px] leading-18px text-[#73808D] font-normal">
+                {/* mail */}
+                <a href="mailto:shanisinojiya@gmail.com">
+                  shanisinojiya@gmail.com
+                </a>
+              </div>
+              <div className="text-[11px] leading-18px text-[#73808D] font-normal">
+                {/* Phone Number */}
+                <a href="tel:+91 63539 40369">+91 63539 40369</a>
+              </div>
+              <div className="text-[11px] leading-18px text-[#73808D] font-normal">
+                {/* location */}
+                Surat, Gujrat, India
+              </div>
+              <br />
+            </div>
+            <div>
+              {/* social network */}
+              <div className="text-[11px] leading-18px text-[#73808D] font-normal">
+                {/* mail */}
+                <a
+                  href="https://www.facebook.com/shani.sinojiya"
+                  className="inline-flex justify-center"
+                  target="_blank"
+                  rel="Shani Sinojiya"
+                >
+                  <i className="bi bi-facebook pr-2"></i> Shani Sinojiya
+                </a>
+              </div>
+              <div className="text-[11px] leading-18px text-[#73808D] font-normal">
+                {/* mail */}
+                <a
+                  href="https://www.twitter.com/ShaniSinojiya"
+                  className="inline-flex justify-center"
+                  target="_blank"
+                  rel="Shani Sinojiya"
+                >
+                  <i className="bi bi-twitter pr-2"></i> Shani Sinojiya
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Theme_1;
